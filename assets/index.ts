9dad542df8dfbd563bd07541a1b34fe15b0ec9f6
clive-cudi <clivemaina41@gmail.com>
2023-07-10 10:58:15 +0300

    modularized-card-data

---
+++ assets/index.ts
@@ -0,0 +1,19 @@
+export interface IPerson {
+    name: string;
+    age: number;
+    image: string;
+    village: string;
+    primary_sch: string;
+    description: string;
+  }
+
+export const persons: IPerson[] = [
+    {
+      name: "Veronica Maloi Musuni",
+      age: 16,
+      image: "/images/home/veronica-2.jpeg",
+      village: "Nkoisuash",
+      primary_sch: "Nkoisuash",
+      description: "_"
+    }
+];
--- components/sections/CardsSection.tsx
+++ components/sections/CardsSection.tsx
@@ -1,26 +1,8 @@
 import styles from "@/app/page.module.scss";
 import { Card } from "../Card";
-
-export interface IPerson {
-  name: string;
-  age: number;
-  image: string;
-  village: string;
-  primary_sch: string;
-  description: string;
-}
+import { persons as cards } from "@/assets";
 
 export const CardsSection = (): JSX.Element => {
-  const cards: IPerson[] = [
-    {
-      name: "Veronica Maloi Musuni",
-      age: 16,
-      image: "/images/home/veronica-2.jpeg",
-      village: "Nkoisuash",
-      primary_sch: "Nkoisuash",
-      description: "_"
-    }
-  ];
     return (
         <section className={`${styles.page_section} ${styles.cards_section}`}>
           <div className={styles.page_section_content}>
