ae07725e730fcea9c0aa96dca3974c5675ccc380
clive-cudi <clivemaina41@gmail.com>
2023-07-10 11:05:02 +0300

    fixed-build-error-type-import-IPerson

--- components/sections/CardsSection.tsx
+++ components/sections/CardsSection.tsx
@@ -2,6 +2,15 @@ import styles from "@/app/page.module.scss";
 import { Card } from "../Card";
 import { persons as cards } from "@/assets";
 
+export interface IPerson {
+  name: string;
+  age: number;
+  image: string;
+  village: string;
+  primary_sch: string;
+  description: string;
+}
+
 export const CardsSection = (): JSX.Element => {
     return (
         <section className={`${styles.page_section} ${styles.cards_section}`}>
