05f23a4831afc4a10e17435edb9b692d839e3331
Timpiyian <129653241+Timpiyian@users.noreply.github.com>
2023-07-11 18:48:21 +0300

    added more students

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,6 @@
 export interface IPerson {
     name: string;
-    age: number;
+    age: CharacterData;
     image: string;
     village: string;
     primary_sch: string;
@@ -16,4 +16,322 @@ export const persons: IPerson[] = [
       primary_sch: "Nkoisuash",
       description: "_"
     }
+    {
+      name: "Anne Peyiai Ndonyuo",
+      age: 13,
+      image: "/images/Ann peyiai story.jpg",
+      village: "Ilkesumeti",
+      primary_sch: "Esupetai",
+      description: "_"
+    }
+    {
+      name: "Elizabeth Nakoyian kinayio",
+      age: 15,
+      image: "/images/Eliza nakoyian story.jpg",
+      village: "salama",
+      primary_sch: "Nkairramiram",
+      description: "_"
+    }
+    {
+      name: "Joyce Resian Keshe",
+      age: 13,
+      image: "/images/Joyce keshe story.jpg",
+      village: "Nkoisuash",
+      primary_sch: "Nkoisuash",
+      description: "_"
+    }
+    {
+      name: "Joyce Resian Keshe",
+      age: 13,
+      image: "/images/Joyce keshe story.jpg",
+      village: "Nkoisuash",
+      primary_sch: "Nkoisuash",
+      description: "_"
+    }
+    {
+      name: "Abigail Kosen",
+      age: ,
+      image: "/images/Abigail Kosen.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Caren Naitoe",
+      age: ,
+      image: "/images/Caren Naitoe.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+      
+    }
+    {
+      name: "Diana Keshe",
+      age: ,
+      image: "/images/Diana Keshe.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Elizabeth kariankei",
+      age: ,
+      image: "/images/Elizabeth kariankei.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Elizabeth nkoitiko",
+      age: ,
+      image: "/images/elizabeth nkoitiko.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Esther maso",
+      age: ,
+      image: "/images/Esther maso.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Esther pesi",
+      age: ,
+      image: "/images/Esther pesi.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Faith solio",
+      age: ,
+      image: "/images/Faith solio.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Gladys nkoyieyio",
+      age: ,
+      image: "/images/Gladys nkoyieyio.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Gladys seuri",
+      age: ,
+      image: "/images/gladys seuri.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Gladys,Elizabeth and shirleen",
+      age: ,
+      image: "/images/Gladys,Elizabeth and shirleen.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Hellen Nambayio",
+      age: ,
+      image: "/images/Hellen Nambayio.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Irene Rotiken",
+      age: ,
+      image: "/images/Irene Rotiken.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+
+    {
+      name: "Jacklyne Nchoe",
+      age: ,
+      image: "/images/Jacklyne Nchoe.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Jane kesier",
+      age: ,
+      image: "/images/jane kesier.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Jessica Saoli",
+      age: ,
+      image: "/images/jessica saoli.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+
+    {
+      name: "Josphine kariankei",
+      age: ,
+      image: "/images/josphine kariankei.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Josphine Nkoile",
+      age: ,
+      image: "/images/josphine nkoile.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+
+    {
+      name: "Josphine pion",
+      age: ,
+      image: "/images/Josphine pion.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+
+    {
+      name: "Joy Nkapiani",
+      age: ,
+      image: "/images/Joy Nkapiani.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Joyce keshe",
+      age: ,
+      image: "/images/Joyce keshe.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Melan Tompo",
+      age: ,
+      image: "/images/Melan Tompo.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Mercy kinayio",
+      age: ,
+      image: "/images/Mercy kinayio.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Mitchel mootian",
+      age: ,
+      image: "/images/Mitchel mootian.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Naishorua Taki",
+      age: ,
+      image: "/images/Naishorua Taki.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Namunyak koonyo",
+      age: ,
+      image: "/images/Namunyak koonyo.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Priscilla Namunyak Parate",
+      age: ,
+      image: "/images/Priscilla Namunyak Parate.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Renny Titany",
+      age: ,
+      image: "/images/Renny Titany.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Resiato Kerempe",
+      age: ,
+      image: "/images/Resiato Kerempe.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Salaite koonyi",
+      age: ,
+      image: "/images/salaite koonyi.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Sheila Kerempe",
+      age: ,
+      image: "/images/Sheila Kerempe.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Shirleen kapoto",
+      age: ,
+      image: "/images/shirleen kapoto.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Silvia Lanoi",
+      age: ,
+      image: "/images/Silvia Lanoi.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Timanoi pion",
+      age: ,
+      image: "/images/Timanoi pion.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    {
+      name: "Vivian Nanteya",
+      age: ,
+      image: "/images/Vivian Nanteya.jpg",
+      village: "",
+      primary_sch: "",
+      description: "_"
+    }
+    
 ];
