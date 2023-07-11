b668aa877fa8522cec7e2113d9add04edf8fd2fc
clive-cudi <clivemaina41@gmail.com>
2023-07-11 22:02:40 +0300

    finished-on-person-modal

--- components/PersonInfoPopup.tsx
+++ components/PersonInfoPopup.tsx
@@ -1,5 +1,6 @@
 import { IPerson } from "@/assets";
 import styles from "@/styles/components/personInfopopup.module.scss";
+import Image from "next/image";
 
 interface IPersonInfoPopup {
     info: IPerson
@@ -10,10 +11,31 @@ export const PersonInfoPopup = ({info}: IPersonInfoPopup): JSX.Element => {
         <div className={styles.pip_wrapper}>
             <div className={styles.pip_content}>
                 <div className={styles.pip_strip}>
-                    {/* image here */}
+					<div className={styles.bg_image} id="image-wrapper" style={{backgroundImage: `url('${info.image}')`}}></div>
+                    <div className={styles.pip_strip_img}>
+                        {/* image here */}
+                        <Image src={info.image} alt={`@${info.name}_pic`} fill /> 
+                    </div>       
                 </div>
-                <div className={styles.pip_strip}></div>
-                <div className={styles.pip_strip}></div>
+                <div className={styles.pip_strip}>
+                    {/* name */}
+                    <h3>{info.name}</h3>
+					<hr />
+                </div>
+                <div className={styles.pip_strip}>
+                    {/* bio */}
+                    <ul>
+                        <li><strong>Age: </strong>{info.age}</li>
+                        <li><strong>Village: </strong>{info.village}</li>
+                        <li><strong>Primary School: </strong>{info.primary_sch}</li>
+                    </ul>
+                </div>
+                <div className={styles.pip_strip}>
+                    <p>{info.description}</p>
+                </div>
+                <div className={styles.pip_strip}>
+                    <button>View Story</button>
+                </div>
             </div>
         </div>
     )
